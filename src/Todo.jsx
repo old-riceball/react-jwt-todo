7d02c5f37bb748efaec4e0c7b0df87438145084b
Riceball <42161586+Anderice@users.noreply.github.com>
2022-07-20 07:42:43 +0800

    fix tasks cloudtasks edit problem & clear all btn

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,3 +1,5 @@
+// 不希望在切換 Tab 的時候還要去 fetch 資料，因此除了 Tasks 外還設置了 cloudTasks 來紀錄雲端上應有的資料(也就是目前全部資料)。
+
 import { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import useLogout from './hooks/useLogout'
@@ -5,23 +7,30 @@ import useGetTodo from './hooks/useGetTodo'
 import useToggleTodo from './hooks/useToggleTodo'
 import useAddTodo from './hooks/useAddTodo'
 import useRemoveTodo from './hooks/useRemoveTodo'
+import useRemoveAllTodo from './hooks/useRemoveAllTodo'
+import writer from './assets/writer.png'
 
 const Todo = () => {
     const navigate = useNavigate()
     const nickName = localStorage.getItem("nickname") || "陌生人"
     const token = localStorage.getItem('token')
+    const tasksTab = document.querySelector('[data-todo-tab]')
     const [input, setInput] = useState('')
     const [tasks, setTasks] = useState([])
     const [cloudTasks, setCloudTasks] = useState([])
     const [isLoadedTasks, setIsLoadedTasks] = useState(false)
     const [showClearBtn, setShowClearBtn] = useState(false)
     const [completedCount, setCompletedCount] = useState(0)
+    const [activeTab, setActiveTab] = useState('')
+
 
     useEffect(() => { init() }, [])
 
     useEffect(() => {
-        console.log('tasks rendered');
-        const completedTasks = cloudTasks.filter((item) => {
+        async () => {
+            setCloudTasks(await useGetTodo(token))
+        }
+        const completedTasks = tasks.filter((item) => {
             return !item.completed_at
         })
         completedTasks.length > 0 ? setShowClearBtn(false) : setShowClearBtn(true)
@@ -35,16 +44,21 @@ const Todo = () => {
         setIsLoadedTasks(true)
     }
 
-    const handleClearCompleted = async() => {
-        const newTasks = [...tasks]
-        const uncompletedTasks = newTasks.filter((item, index) => {
+    const handleClearCompletedTasks = () => {
+        const ids = []
+        const newCloudTasks = cloudTasks.filter(item => {
             if (item.completed_at){
-                console.log(index, item.id);
-                removeTask(index, item.id)
+                ids.push(item.id)
             }
             return !item.completed_at
         })
-        setTasks(uncompletedTasks)
+        useRemoveAllTodo(token, ids)
+
+        
+
+        setCloudTasks(newCloudTasks)
+        setTasks(newCloudTasks)
+
     }
 
     const handleSubmit = (e) => {
@@ -62,46 +76,74 @@ const Todo = () => {
         const data = await useToggleTodo(token, id)
         newTasks[index].completed_at = data.completed_at
         setTasks(newTasks)
+        setCloudTasks(newTasks)
     }
 
     const removeTask = async (index, id) => {
         const newTasks = [...tasks]
+
+        // Remove task from tasks
         const response = await useRemoveTodo(token, id)
         newTasks.splice(index, 1)
         setTasks(newTasks)
+        
+        //  Remove task from cloudTasks
+        const newCloudTasks = cloudTasks.filter(item => {
+            return !(item.id === id)
+        })
+        setCloudTasks(newCloudTasks)
     }
 
     const addTask = async (content) => {
         const response = await useAddTodo(token, content)
-        setTasks([...tasks, response])
+        const newTask = {
+            content: response.content,
+            id: response.id,
+            completed_at: null //Response 並沒有給這項屬性，自己加上
+        }
+        showAllTasks()
+        setTasks([...cloudTasks, newTask])
+        setCloudTasks([...cloudTasks, newTask])
     }
     
-    const handleGroup = (e) => {
-        e.target.parentElement.querySelectorAll('button').forEach(item => {
-            item.classList.remove('todo__tab--active')
-        })
+    const handleTabs = (e) => {
+        clearAllTabs()
         e.target.classList.add('todo__tab--active')
 
         switch (e.target.innerText) {
             case "全部":
                 setTasks(cloudTasks)
+                setActiveTab('')
                 break
             case "待完成":
- 
                 const uncompletedTasks = cloudTasks.filter((item) => {
                     return !item.completed_at
                 })
                 setTasks(uncompletedTasks)
+                setActiveTab('待完成')
                 break
             case "已完成":
-
                 const completedTasks = cloudTasks.filter((item) => {
                     return item.completed_at
                 })
 
                 setTasks(completedTasks)
+                setActiveTab('已完成')
                 break
         }
+    }
+
+    const clearAllTabs = () => {
+        tasksTab.querySelectorAll('button').forEach(item => {
+            item.classList.remove('todo__tab--active')
+        })
+    }
+
+    const showAllTasks = () => {
+        const allTab = document.querySelector('[data-tab-all]')
+        clearAllTabs()
+        allTab.classList.add('todo__tab--active')
+        setTasks(cloudTasks)
     }
 
     return (
@@ -124,7 +166,7 @@ const Todo = () => {
             </nav>
             <div className="w-full max-w-[500px]">
                 <form onSubmit={handleSubmit} className="flex items-center bg-white shadow-md rounded-lg mb-4">
-                    <input onChange={e => setInput(e.target.value)} className="w-full px-4 py-3 rounded-lg" type="text" placeholder="新增待辦事項" value={input} />
+                    <input onChange={e => setInput(e.target.value)} className="w-full px-4 py-3 rounded-lg outline-none" type="text" placeholder="新增待辦事項" value={input} required />
                     <button className="mx-1 bg-gray-700 w-10 aspect-square rounded-lg  right-0 relative">
                         <svg className="absolute right-1/2 top-1/2 transform-gpu -translate-y-1/2 translate-x-1/2" width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M18.6364 8.93184H11.8182V2.11364C11.8182 1.36091 11.2073 0.75 10.4545 0.75H9.54548C8.79275 0.75 8.18184 1.36091 8.18184 2.11364V8.93184H1.36364C0.610908 8.93184 0 9.54275 0 10.2955V11.2045C0 11.9573 0.610908 12.5682 1.36364 12.5682H8.18184V19.3864C8.18184 20.1391 8.79275 20.75 9.54548 20.75H10.4545C11.2073 20.75 11.8182 20.1391 11.8182 19.3864V12.5682H18.6364C19.3891 12.5682 20 11.9573 20 11.2045V10.2955C20 9.54275 19.3891 8.93184 18.6364 8.93184Z" fill="white"/>
@@ -133,12 +175,52 @@ const Todo = () => {
                 </form>
 
                 <div className="shadow-md bg-white rounded-lg">
-                    <div onClick={handleGroup} className="flex">
-                        <button className="flex-1 opacity-40 font-bold py-4 border-b-2 todo__tab--active tracking-widest">全部</button>
-                        <button className="flex-1 opacity-40 font-bold py-4 border-b-2 tracking-widest">待完成</button>
-                        <button className="flex-1 opacity-40 font-bold py-4 border-b-2 tracking-widest">已完成</button>
+                    <div className="fixed bottom-0 right-0">
+                        <div className="bg-red-300 p-8">
+                            <h2 className="font-bold text-lg">Tasks</h2>
+                            {tasks.map(task => {
+                                
+                                    return(
+                                    <ul>
+                                        <li>
+                                            {task.content} - {task.completed_at}
+                                        </li>
+                                    </ul>)
+                                
+                            })}
+                        </div>
+                        <div className="bg-blue-300 p-8">
+                            <h2 className="font-bold text-lg">CloudTasks</h2>
+                            {cloudTasks.map(task => {
+                                return(
+                                <ul>
+                                    <li>
+                                        {task.content} - {task.completed_at}
+                                    </li>
+                                </ul>)
+                            })}
+                        </div>
                     </div>
-                    { !isLoadedTasks && <p className="m-8 text-lg text-center text-gray-600">歡迎登入 {nickName}，<br></br>您的個人化資料正加載中</p>}
+
+
+                    <div data-todo-tab onClick={handleTabs} className="flex">
+                        <button data-tab-all className="flex-1 text-gray-800 text-opacity-40 font-bold py-4 border-gray-800 border-opacity-10 border-b-2 todo__tab--active tracking-widest">全部</button>
+                        <button className="flex-1 text-gray-800 text-opacity-40 font-bold py-4 border-b-2 border-gray-800 border-opacity-10 tracking-widest">待完成</button>
+                        <button className="flex-1 text-gray-800 text-opacity-40 font-bold py-4 border-b-2 border-gray-800 border-opacity-10 tracking-widest">已完成</button>
+                    </div>
+
+                    <div className="border-b-2 border-gray-800 border-opacity-10">
+                        { !isLoadedTasks && <p className="m-8 text-lg text-center text-gray-600">歡迎登入 {nickName}，<br></br>您的個人資料正加載中……</p>}
+    
+                        { !tasks.length && isLoadedTasks ? 
+                        <div className="flex flex-col items-center my-8">
+                            <img width="240" height="251" src={writer} alt="一個人拿著鉛筆寫事項"/>
+                            <p className="m-8 text-lg text-center text-gray-600">尚無{ activeTab }代辦事項</p>
+                        </div>
+                        : <></>}
+                    </div>     
+
+
                     <ul>
                         {tasks.map((task, index) => {
                             return (
@@ -149,9 +231,11 @@ const Todo = () => {
                                     className="checkbox appearance-none w-5 h-5 rounded mr-4 border-2 border-gray-300 cursor-pointer"
                                     id={index}
                                     type="checkbox"
-                                    defaultChecked={task.completed_at} />
+                                    // defaultChecked={task.completed_at} // 詭異的屬性，讓我卡好久 QQ https://blog.csdn.net/TL18382950497/article/details/115425569
+                                    // checked={task.completed_at}
+                                    />
 
-                                    {task.content} {task.completed_at}
+                                    {task.content}
 
                                     <div className="check opacity-0 absolute left-0">
                                         <svg className="mt-1.5" width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -169,7 +253,7 @@ const Todo = () => {
                     </ul>
                     <div className="flex justify-between items-center px-6">
                         <p>{completedCount} 個待完成項目</p>
-                        <button onClick={handleClearCompleted} disabled={showClearBtn} className="disabled:opacity-60 py-6">清除已完成項目</button>
+                        <button onClick={handleClearCompletedTasks} disabled={showClearBtn} className="disabled:opacity-60 py-6">清除已完成項目</button>
                     </div>
                 </div>
             </div>
---
+++ src/hooks/useRemoveAllTodo.jsx
@@ -0,0 +1,19 @@
+const useRemoveAllTodo = async(token, ids) => {
+  const requestOptions = {
+    method: 'DELETE',
+    headers: {
+       'Content-Type': 'application/json',
+       'Authorization': token
+      },
+  }
+
+  const APIs = ids.map(id => 'https://todoo.5xcamp.us/todos/'+ id)
+
+  Promise.all(APIs.map( async url => {
+    const response = await fetch(url, requestOptions)
+    const responseJson = await response.json()
+    console.log(response, responseJson);
+  }))
+}
+
+export default useRemoveAllTodo
