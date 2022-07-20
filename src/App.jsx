b05c1c4a698944cc6bbf34b840c581d60293e237
Riceball <42161586+Anderice@users.noreply.github.com>
2022-07-21 02:34:35 +0800

    path fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Error from './Error'
 import Home from './Home'
@@ -8,19 +7,18 @@ import Todo from './Todo'
 import ProtectedRoute from './ProtectedRoute'
 import toast, { Toaster } from 'react-hot-toast';
 
-
 const isLoggin = localStorage.getItem('token')
 
 function App() {
   return (
     <div className="min-h-screen bg-primary flex justify-center items-center px-4">
-      <BrowserRouter>
+      <BrowserRouter basename="/react-jwt-todo/">
         <Routes>
           <Route path="*" element={<Error />}/> 
           <Route path="/" element={<Home />} />
-          <Route path="register" element={<Register />}/>
-          <Route path="login" element={<Login />}/>
-          <Route path="todo" element={
+          <Route path="/register" element={<Register />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/todo" element={
             <ProtectedRoute isLoggin={isLoggin}>
               <Todo />
             </ProtectedRoute>
